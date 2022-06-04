68ce3c3543381c518b75d55ededb993eacc79527
Austin Brock <austinjbrock@gmail.com>
2022-06-03 19:10:56 -0700

    added random color functionality

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 //establish a variable so we can change it later
 let color = 'black'
 let input = document.querySelector('input')
+const hexValues = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']; 
+
 
 //Rest Board size
 resetBtn = document.getElementById('reset')
@@ -24,29 +26,43 @@ for(let i =0; i <256; i++){
 
 //default is black until button on html is clicked
 function styleBox(){
-   this.style.backgroundColor = color;
-   
-};
+   //generates a random color
+   if(color == 'random'){
+   let hex = '#';
+   //loop through the hex table and return a 6 digit hex number
+   for(let i =0; i <6; i++){  
+   const index = Math.floor(Math.random()* hexValues.length)
+   hex += hexValues[index]
+}
+   this.style.backgroundColor = hex
+}
+   else {
+      this.style.backgroundColor = color;
+   }
+}
 
 //based on which button is clicked, this will pass in our color choice for the stylebox function
 function changeDrawColor (choice){
    color = choice;
 }
 
-// this function is called in the html structure
+
+//sets div cube size
 function changeSize(input){
-   if(input >=2 && input <=100){
+   if(input >=2 && input <=20){
       buildBoard(input);
    }else {
-      alert('Enter a number from 2 - 100')
+      alert('Enter a number from 2 - 20')
    }
 }
 
+//reset block dimensions
 function resetBoard(){
-   buildBoard()
+   buildBoard(16)
    input.value = ''
 }
 
 reset.addEventListener('click',resetBoard)
 
+//default board size
 buildBoard(16);
